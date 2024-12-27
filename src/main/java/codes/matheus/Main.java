493a7f9fb4954eb42decbaebc51a463ab7a2ed2f
matheus <mc2019750@gmail.com>
2024-12-27 17:32:57 -0300

    adding UI

--- src/main/java/codes/matheus/Main.java
+++ src/main/java/codes/matheus/Main.java
@@ -1,11 +1,16 @@
 package codes.matheus;
 
-import codes.matheus.game.Game;
+import codes.matheus.ui.Window;
 import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        @NotNull Game game = new Game();
-        game.run();
+//        @NotNull Game game = new Game();
+//        game.run();
+        @NotNull Window window = new Window();
+        JPanel panel = new JPanel();
+        window.add(panel);
     }
 }
--- src/main/java/codes/matheus/ui/Window.java
+++ src/main/java/codes/matheus/ui/Window.java
@@ -5,10 +5,11 @@ import java.awt.*;
 
 public final class Window extends JFrame {
     public Window() throws HeadlessException {
-        setSize(1920, 1080);
+        setSize(1000, 600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(true);
         setFocusable(true);
+        setVisible(true);
     }
 }
